07c28bc2b558067f5a191bb412a8a30c5c5ef14b
abudak <acbudak2005@gmail.com>
2022-07-26 13:22:58 -0400

    network

--- social_network_classes1.py
+++ social_network_classes1.py
@@ -3,6 +3,8 @@
 class SocialNetwork:
     def __init__(self):
         self.user_list1 = []
+        
+        
 
 
 class Person:
@@ -10,12 +12,20 @@ class Person:
         self.id = name
         self.password = password
         self.age = age
-        self.friendlist = []
+     
 
-    def add_friend(self, person_object):
+    def user_information(self):
+        print("name:", self.id)
+        print("password:", self.password)
+        print("age:",self.age)
+
+    def add_friend(self,friends):
+        self.friend_list = []
+        self.friends = friends
+        print("friends" , self.friends)
+       
         #implement adding friend. Hint add to self.friendlist
-        pass
-
+        
     def send_message(self):
         #implement sending message to friend here
         pass
--- user_interface.py
+++ user_interface.py
@@ -13,6 +13,14 @@ def mainMenu():
     print("2. Manage my account")
     print("3. Quit")
     print("********************************************************")
+
+def profile():
+    print("")
+    print("1. Edit my profile")
+    print("2. Add a friend")
+    print("3. View all my friends")
+    print("4. View all my messages")
+    print("5. <- Go back ")
      
 
 
@@ -23,10 +31,11 @@ while True:
     if response1 == "1":
         print("You are now in the create account menu")
 
-        new_username=input("Please select a username:")
-        new_password=input("Please select a password:")
+        new_username=input("Please select your username:")
+        new_password=input("Please select your password:")
+        new_age=input("Plese select age:")
 
-        new_user1=classes.Person(new_username, new_password, "Unknown")
+        new_user1=classes.Person(new_username, new_password, new_age)
         network1.user_list1.append(new_user1)
         continue
 
@@ -39,35 +48,52 @@ while True:
         for x in network1.user_list1:
             if x.id == login_username and x.password== login_password :
                 user=x
-     
-                def profile():
-                    print("")
-                    print("1. Edit my profile")
-                    print("2. Add a friend")
-                    print("3. View all my friends")
-                    print("4. View all my messages")
-                    print("5. <- Go back ")
-
-                profile()
-
     while True:
+        profile()
         response2 = input("Please Choose a number: ") 
+        
         if response2 == "1":
-                print("You are now in the edit profile menu")
-                user.login_username=input("Please select a username:")
-                user.login_password=input("Please select a password:")
+            print("You are now in the edit profile menu")
+            user.id=input("Please reset a username:")
+            user.password=input("Please reset a password:")
+            user.age=input("Please reset an age:")
+            print("Bravo you have reseted your account")
+
+            user.user_information()
 
 
+        elif x.id != login_username or x.password != login_password :
+            print("this username does not match with the password")
+            mainMenu()
 
- 
+
+    
+
+
+        elif response2 == "2":
+            
+            uptated_friends=input("Please select the username of the friend you wanna add:")
+            friends = classes.Person(uptated_friends)
+            network1.friend_list.append(uptated_friends)
+            for y in network1.friend_list:
+                if y.friends == uptated_friends :
+                    friends = y
+        elif response2 == "5":
+                print("Goodbye")
+                mainMenu()
+                        
+
+            
+            #variable yap= input (add who as friend?)
+            #set the variable to that
+            #user.friendlist.append(var)
 
 
     # new_user1=classes.Person(login_username, login_password, "No matching username or password")
 
-    
     # Invalid response
         elif response1=="3":
-            mainMenu()
+                mainMenu()
 
   
 
